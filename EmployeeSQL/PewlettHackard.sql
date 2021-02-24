3c850cacac1c79c08550dfb56d161e5974348685
Chase Webb <webb.chase.a@gmail.com>
2021-02-24 18:34:08 -0500

    Update PewlettHackard.sql
    Added comments to sql schema.

--- EmployeeSQL/PewlettHackard.sql
+++ EmployeeSQL/PewlettHackard.sql
@@ -1,3 +1,4 @@
+-- Create title table and import data to the table.
 CREATE TABLE Title (
     title_id VARCHAR   NOT NULL,
     title VARCHAR   NOT NULL,
@@ -9,6 +10,7 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\titles.csv'
 DELIMITER ','
 CSV HEADER;
 
+-- Create employee table and import data to the table.
 CREATE TABLE Employee (
     emp_no INT NOT NULL,
 	emp_title_id VARCHAR NOT NULL,
@@ -26,6 +28,7 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\employees.csv'
 DELIMITER ','
 CSV HEADER;
 
+-- Create salaries table and import data to the table.
 CREATE TABLE Salaries (
     emp_no int,
     salary int NOT NULL,
@@ -37,6 +40,7 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\salaries.csv'
 DELIMITER ','
 CSV HEADER;
 
+-- Create department table and import data to the table.
 CREATE TABLE Department (
     dept_no VARCHAR   NOT NULL,
     dept_name VARCHAR   NOT NULL,
@@ -48,6 +52,7 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\departments.csv
 DELIMITER ','
 CSV HEADER;
 
+-- Create departmentmanager table and import data to the table.
 CREATE TABLE DepartmentManager (
     dept_no VARCHAR   NOT NULL,
     emp_no int   NOT NULL
@@ -58,6 +63,7 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\dept_manager.cs
 DELIMITER ','
 CSV HEADER;
 
+-- Create department employees table and import data to the table.
 CREATE TABLE DepartmentEmployees (
     emp_no int   NOT NULL,
 	dept_no VARCHAR   NOT NULL
@@ -68,14 +74,23 @@ FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\dept_emp.csv'
 DELIMITER ','
 CSV HEADER;
 
+-- Add foreign key to salaries from employee table.
 ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
 REFERENCES Employee (emp_no);
 
+-- Add foreign key to department manager from department table.
 ALTER TABLE DepartmentManager ADD CONSTRAINT fk_DepartmentManager_dept_no FOREIGN KEY(dept_no)
 REFERENCES Department (dept_no);
 
+-- Add foreign key to department employees from department table.
 ALTER TABLE DepartmentEmployees ADD CONSTRAINT fk_DepartmentEmployees_dept_no FOREIGN KEY(dept_no)
 REFERENCES Department (dept_no);
 
+-- Add foreign key to department employees from employee table.
 ALTER TABLE DepartmentEmployees ADD CONSTRAINT fk_DepartmentEmployees_emp_no FOREIGN KEY(emp_no)
 REFERENCES Employee (emp_no);
+
+SELECT players.first_name, players.last_name, players.hand, matches.loser_rank
+FROM matches
+INNER JOIN players ON
+players.player_id=matches.loser_id;
