d9eb91f1236de9ab019adda6bf4ad398346adf24
Chase Webb <webb.chase.a@gmail.com>
2021-02-24 00:06:18 -0500

    Planned out sql tables.

---
+++ SourceData/QuickDBD-export.sql
@@ -0,0 +1,58 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Employee" (
+    "EmployeeID" int   NOT NULL,
+    "LastName" VARCHAR   NOT NULL,
+    "FirstName" VARCHAR   NOT NULL,
+    "Sex" VARCHAR   NOT NULL,
+    "HireDate" date   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "EmployeeID"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "EmployeeID" int   NOT NULL,
+    "Salary" money   NOT NULL
+);
+
+CREATE TABLE "Title" (
+    "TitleID" int   NOT NULL,
+    "JobTitle" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Title" PRIMARY KEY (
+        "TitleID"
+     )
+);
+
+CREATE TABLE "Department" (
+    "DepartmentID" int   NOT NULL,
+    "DepartmentName" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Department" PRIMARY KEY (
+        "DepartmentID"
+     )
+);
+
+CREATE TABLE "DepartmentManager" (
+    "DepartmentID" int   NOT NULL,
+    "EmployeeID" int   NOT NULL
+);
+
+CREATE TABLE "DepartmentEmployees" (
+    "DepartmentID" int   NOT NULL,
+    "EmployeeID" int   NOT NULL
+);
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_EmployeeID" FOREIGN KEY("EmployeeID")
+REFERENCES "Employee" ("EmployeeID");
+
+ALTER TABLE "DepartmentManager" ADD CONSTRAINT "fk_DepartmentManager_DepartmentID" FOREIGN KEY("DepartmentID")
+REFERENCES "Department" ("DepartmentID");
+
+ALTER TABLE "DepartmentEmployees" ADD CONSTRAINT "fk_DepartmentEmployees_DepartmentID" FOREIGN KEY("DepartmentID")
+REFERENCES "Department" ("DepartmentID");
+
+ALTER TABLE "DepartmentEmployees" ADD CONSTRAINT "fk_DepartmentEmployees_EmployeeID" FOREIGN KEY("EmployeeID")
+REFERENCES "Employee" ("EmployeeID");
+
