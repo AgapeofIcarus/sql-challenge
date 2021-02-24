aacac4ad0678769dddda39c507c482c4eeab296a
Chase Webb <webb.chase.a@gmail.com>
2021-02-24 03:28:52 -0500

    Created tables
    Imported csvs to create sql tables.

---
+++ EmployeeSQL/PewlettHackard.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Employee (
+    emp_no INT NOT NULL,
+	emp_title VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+    first_name VARCHAR   NOT NULL,
+	last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+	CONSTRAINT pk_Employee PRIMARY KEY (emp_no)
+);
+
+COPY Employee(emp_no, emp_title, birth_date, first_name, last_name, sex, hire_date)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE Salaries (
+    emp_no int,
+    salary int NOT NULL,
+	CONSTRAINT pk_Salaries PRIMARY KEY (emp_no, salary)
+);
+
+COPY Salaries(emp_no, salary)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\salaries.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE Title (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_Title PRIMARY KEY (title_id)
+);
+
+COPY Title(title_id, title)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE Department (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Department PRIMARY KEY (dept_no)
+);
+
+COPY Department(dept_no, dept_name)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE DepartmentManager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no int   NOT NULL
+);
+
+COPY DepartmentManager(dept_no, emp_no)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE DepartmentEmployees (
+    emp_no int   NOT NULL,
+	dept_no VARCHAR   NOT NULL
+);
+
+COPY DepartmentEmployees(emp_no, dept_no)
+FROM 'C:\Users\Captain\Documents\GitHub\sql-challenge\SourceData\dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE DepartmentManager ADD CONSTRAINT fk_DepartmentManager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE DepartmentEmployees ADD CONSTRAINT fk_DepartmentEmployees_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE DepartmentEmployees ADD CONSTRAINT fk_DepartmentEmployees_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
