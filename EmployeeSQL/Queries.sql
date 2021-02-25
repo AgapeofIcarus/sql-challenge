ac9aa310df49d7a9bd25bd63b5ae6a0a93a6db0e
Chase Webb <webb.chase.a@gmail.com>
2021-02-24 19:27:11 -0500

    Update Queries.sql
    Generated list for employees hired in 1986.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -3,3 +3,8 @@ SELECT Employee.emp_no, Employee.last_name, Employee.first_name, Employee.sex, S
 FROM Employee
 JOIN salaries ON
 Employee.emp_no=Salaries.emp_no;
+
+--Create list containing first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employee
+WHERE hire_date between '1986-01-01' and '1986-12-31'
