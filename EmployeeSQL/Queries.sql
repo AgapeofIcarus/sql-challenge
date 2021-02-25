59502955817056363c115c848521f918f4f47fb9
Chase Webb <webb.chase.a@gmail.com>
2021-02-24 20:47:37 -0500

    Update Queries.sql
    Did formatting to fit personal preference, completed list five query for project.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -7,17 +7,23 @@ Employee.emp_no=Salaries.emp_no;
 --Create list containing first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employee
-WHERE hire_date between '1986-01-01' and '1986-12-31'
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 
 --Create list containing manager of each department with the following information: department number, 
 --department name, the manager's employee number, last name, first name.
 SELECT departmentmanager.dept_no, department.dept_name, departmentmanager.emp_no, employee.last_name, employee.first_name
-from departmentmanager
-   inner join department on departmentmanager.dept_no = department.dept_no
-   inner join employee on departmentmanager.emp_no = employee.emp_no
+FROM departmentmanager
+   INNER JOIN department ON departmentmanager.dept_no = department.dept_no
+   INNER JOIN employee ON departmentmanager.emp_no = employee.emp_no
    
 --Create list containing employee number, last name, first name, and department name.
 SELECT departmentemployees.emp_no, employee.last_name, employee.first_name, department.dept_name
-from employee
-   inner join departmentemployees on employee.emp_no = departmentemployees.emp_no
-   inner join department on departmentemployees.dept_no = department.dept_no
+FROM employee
+   INNER JOIN departmentemployees ON employee.emp_no = departmentemployees.emp_no
+   INNER JOIN department ON departmentemployees.dept_no = department.dept_no
+   
+--Create list containing first name, last name, and sex for employees whose first name is "Hercules" 
+--and last names begin with "B."
+SELECT employee.first_name, employee.last_name, employee.sex
+FROM employee
+WHERE employee.first_name = 'Hercules' AND employee.last_name LIKE 'B%'
